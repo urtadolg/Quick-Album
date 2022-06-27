79387b44747db57cf7d4025224850dcdff5d9b01
Leandro <leandro.urtado25@gmail.com>
2022-06-26 21:37:11 -0300

    backdrop styles

--- src/components/modal/ImageDetails.tsx
+++ src/components/modal/ImageDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import styles from "./ImageDetails.module.scss";
 import { useAppDispatch, useAppSelector } from "../../store/hook";
